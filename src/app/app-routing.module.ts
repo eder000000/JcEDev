a241a94c2bf992aea1e042e84f21cdeb0f6de3cc
Eder Garcia <eder000000@gmail.com>
2021-10-28 15:24:00 -0500

    Adding base Routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { LoginComponent } from "./auth/login/login.component";
+import { SignupComponent } from "./auth/signup/signup.component";
+import { CatalogComponent } from "./catalog/catalog.component";
+import { HomeComponent } from "./home/home.component";
+
+const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'signup', component: SignupComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'catalog', component: CatalogComponent },
+
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)], 
+    exports: [RouterModule]
+})
+export class AppRoutingModule{
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MaterialModule } from './material.module';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSliderModule } from '@angular/material/slider';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { RegisterProComponent } from './catalog/register-pro/register-pro.component';
+import { ListProComponent } from './catalog/list-pro/list-pro.component';
+import { ProDetailsComponent } from './catalog/pro-details/pro-details.component';
+import { CatalogComponent } from './catalog/catalog.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    RegisterProComponent,
+    ListProComponent,
+    ProDetailsComponent,
+    CatalogComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatSliderModule
+    MaterialModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
