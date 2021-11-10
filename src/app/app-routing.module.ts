84d1911e00621cd97d8954455b475c43868bcd6b
Eder Garcia <eder000000@gmail.com>
2021-11-10 12:00:29 -0600

    Adding new routes for registering a  profesional

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { RouterModule, Routes } from "@angular/router";
 import { LoginComponent } from "./auth/login/login.component";
 import { SignupComponent } from "./auth/signup/signup.component";
 import { CatalogComponent } from "./catalog/catalog.component";
+import { ListProComponent } from "./catalog/list-pro/list-pro.component";
+import { ProDetailsComponent } from "./catalog/pro-details/pro-details.component";
 import { RegisterProComponent } from "./catalog/register-pro/register-pro.component";
 import { HomeComponent } from "./home/home.component";
 
@@ -13,6 +15,10 @@ const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'catalog', component: CatalogComponent },
     { path: 'register', component: RegisterProComponent},
+    { path: 'listado', component: ListProComponent},
+    { path: 'detalle', component: ProDetailsComponent},
+
+    
 
 ];
 
