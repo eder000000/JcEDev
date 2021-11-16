8b6d9d44a50eb4d09f75f229dbdaec5dc9f341e9
Eder Garcia <eder000000@gmail.com>
2021-11-16 15:16:36 -0600

    Expansion panel for profesional details view done

--- src/app/catalog/list-pro/list-pro.component.ts
+++ src/app/catalog/list-pro/list-pro.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-list-pro',
-  templateUrl: './list-pro.component.html',
-  styleUrls: ['./list-pro.component.css']
+	selector: 'app-list-pro',
+	templateUrl: './list-pro.component.html',
+	styleUrls: [ './list-pro.component.css' ]
 })
 export class ListProComponent implements OnInit {
+	panelOpenState = false;
 
-  constructor() { }
+	constructor() {}
 
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {}
 }
