c212560c4464dc5948d3118613eaaa150f882478
Eder Garcia <eder000000@gmail.com>
2021-11-19 19:18:31 -0600

    Fixing responsive issues in homepage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: [ './home.component.css' ]
 })
 export class HomeComponent implements OnInit {
+	textPresentationCard = 'Jalisco con Empleo te conecta con los profesionales para todos tus proyectos de construcción, mejora, y/o reparación.';
+	imageConstruction = './assets/img/undraw_under_construction_46_principal.png';
+	imageSuscription = './assets/img/undraw_coffee_break_h3uu_thumbnails.png';
+	textSuscriptionCard = 'Soy un profesional, ¿Cómo puedo suscribirme?';
+	constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {}
 }
