c66b2a87422e242d7c1a3a25e524b53fd867ec6e
Eder Garcia <eder000000@gmail.com>
2021-12-11 00:25:38 -0600

    Run build command

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/jc-eapp'));
+app.use(express.static(__dirname + '/dist/JcEApp'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/src/index.html'));});
+'/dist/JcEApp/index.html'));});
 app.listen(process.env.PORT || 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
