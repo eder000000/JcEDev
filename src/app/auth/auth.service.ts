83e76ca1419035473ae372e1fffde130d29f7af2
Eder Garcia <eder000000@gmail.com>
2021-12-11 00:36:28 -0600

    Fix import statement of Subject in auth.service.ts file

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,50 +1,49 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { Subject } from 'rxjs/Subject';
+import { Subject } from 'rxjs';
 
-import { AuthData } from "./auth-data.model";
-import { User } from "./user.module";
+import { AuthData } from './auth-data.model';
+import { User } from './user.module';
 
 @Injectable()
-export class AuthService{
-    authChange = new Subject<boolean>();
-    private user: User;
+export class AuthService {
+	authChange = new Subject<boolean>();
+	private user: User;
 
-    constructor(private router: Router){}
+	constructor(private router: Router) {}
 
-    registerUser(authData: AuthData){
-        this.user = {
-            email: authData.email,
-            userId: Math.round(Math.random() * 10000).toString()
-        };
-        this.authSuccesfully();
-    }
+	registerUser(authData: AuthData) {
+		this.user = {
+			email: authData.email,
+			userId: Math.round(Math.random() * 10000).toString()
+		};
+		this.authSuccesfully();
+	}
 
-    login(authData: AuthData){
-        this.user = {
-            email: authData.email,
-            userId: Math.round(Math.random() * 10000).toString()
-        };
-      this.authSuccesfully();
-    }
+	login(authData: AuthData) {
+		this.user = {
+			email: authData.email,
+			userId: Math.round(Math.random() * 10000).toString()
+		};
+		this.authSuccesfully();
+	}
 
-    logout(){
-        this.user = null;
-        this.authChange.next(false);
-        this.router.navigate(['/login']);
-    }
+	logout() {
+		this.user = null;
+		this.authChange.next(false);
+		this.router.navigate([ '/login' ]);
+	}
 
-    getUser(){
-        return { ...this.user};
-    }
+	getUser() {
+		return { ...this.user };
+	}
 
-    isAuth(){
-        return this.user != null;
-    }
+	isAuth() {
+		return this.user != null;
+	}
 
-    private authSuccesfully(){
-        this.authChange.next(true);
-        this.router.navigate(['/home']);
-        
-    }
+	private authSuccesfully() {
+		this.authChange.next(true);
+		this.router.navigate([ '/home' ]);
+	}
 }
