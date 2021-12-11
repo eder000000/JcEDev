9ea8b29f53bf2887e7c0a5ca099af1699684cc10
Eder Garcia <eder000000@gmail.com>
2021-12-10 18:19:56 -0600

    Fixing PORT

--- server.js
+++ server.js
@@ -5,4 +5,6 @@ app.use(express.static(__dirname + '/dist/jc-eapp'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/jc-eapp/index.html'));});
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
