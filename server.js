1c844f00e6c6858735a4d965332283ecb62162ad
Eder Garcia <eder000000@gmail.com>
2021-12-10 18:51:06 -0600

    Fixing path in server.js file

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/app/dist/jc-eapp'));
+app.use(express.static(__dirname + '/app/jc-eapp'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/app/dist/jc-eapp/index.html'));});
+'/app/jc-eapp/index.html'));});
 app.listen(process.env.PORT || 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
