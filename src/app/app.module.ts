01548965c4aa7cb9ea97ec6ec4a3b327e0858454
edera <edera@amdocs.com>
2022-08-20 01:55:17 -0500

    Adding upper footer section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -37,6 +37,7 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { UserTableDialogComponent } from './user-table/user-table-dialog/user-table-dialog.component';
 import { EditProComponent } from './catalog/edit-pro/edit-pro.component';
+import { FooterMainComponent } from './navigation/footer-main/footer-main.component';
 
 @NgModule({
 	declarations: [
@@ -56,7 +57,8 @@ import { EditProComponent } from './catalog/edit-pro/edit-pro.component';
 		UserTableComponent,
 		PageNotFoundComponent,
 		UserTableDialogComponent, 
-		EditProComponent
+		EditProComponent,
+		FooterMainComponent 
 	],
 	imports: [
 		BrowserModule,
