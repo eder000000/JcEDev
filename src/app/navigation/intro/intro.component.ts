4ffa32cf68b3561a267e71569385dc248acdac22
edera <edera@amdocs.com>
2022-09-02 22:54:46 -0500

    Fix responsive error in intro component

--- src/app/navigation/intro/intro.component.ts
+++ src/app/navigation/intro/intro.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IntroComponent implements OnInit {
   textPresentationCard = 'Jalisco con Empleo te conecta con los profesionales para todos tus proyectos de construcción, mejora, y/o reparación.';
-	imageConstruction = './assets/img/undraw_under_construction_46_principal.png';
+	imageConstruction = './assets/img/undraw_under_construction_46_principal (2).png';
 	imageSuscription = './assets/img/undraw_coffee_break_h3uu_thumbnails.png';
 	textSuscriptionCard = 'Soy un profesional, ¿Cómo puedo suscribirme?'; 
 
