dafb4f5cc990a4fd11d7d79f2782c6c17868a808
PacoRmzA <a01634262@itesm.mx>
2023-03-11 14:57:00 -0600

    Added backend endpoint to cors whitelist

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const app = express();
+const whitelist = ['http://localhost:3000','https://jce-flask-02.herokuapp.com'];
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin) {
+            return callback(null, true);
+        }
+
+        if (whitelist.indexOf(origin) === -1) {
+            var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}
+app.use(cors(corsOptions));
 app.use(express.static(__dirname + '/dist/JcEApp'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
